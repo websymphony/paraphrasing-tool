f7e0c86cef708e1e5904e754ba4845bdd62ca777
websymphony <websymphony@users.noreply.github.com>
2020-12-28 07:16:57 +0000

    Deploying to gh-pages from  @ da6b806a51372386d631a1ee86d90bb053cc5637 🚀

---
+++ _next/static/UnOaVlpCFN-OpKNXs3bBR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-55cc5892aa8948c0a9f6.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-72e0b0f810dae4ca3a25.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
