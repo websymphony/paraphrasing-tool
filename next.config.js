e47ffcabefe8e8f5dd564d68305beb68ae5d3305
Amit Gaur <amitgaur.web@gmail.com>
2020-12-28 17:32:06 +1100

    One more try in correcting the urls

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
-module.exports = {
-  basePath: '/paraphrasing-tool',
-  assetPrefix: '/paraphrasing-tool/'
-}
+const {
+  PHASE_PRODUCTION_BUILD,
+} = require('next/constants');
+
+module.exports = (phase) => {
+  const isProd = phase === PHASE_PRODUCTION_BUILD;
+
+  const env = {
+    NEXT_PUBLIC_BASE_API_URL: (() => {
+      if (isProd) {
+        return 'https://paraphraser.prod.hipcv.com';
+      }
+    })(),
+  };
+
+  // next.config.js object
+  return {
+    basePath: '/paraphrasing-tool',
+    assetPrefix: '/paraphrasing-tool/',
+    env,
+  };
+};
