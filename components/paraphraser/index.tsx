e4878f91a7576e03188ddb4d863194802e8573b3
Amit Gaur <amitgaur.web@gmail.com>
2020-12-29 11:33:26 +1100

    fixes some usability related to the copy and clear text

--- components/paraphraser/index.tsx
+++ components/paraphraser/index.tsx
@@ -46,6 +46,12 @@ export default function Paraphraser(props) {
     toast.success('Copied result in your clipboard. Enjoy!');
   }
 
+  const handleClear = () => {
+    setInputText("");
+    setOutputText("");
+    toast.success('Successfully cleared content.');
+  }
+
   useEffect(() => {
     inputTextRef && inputTextRef.current.focus();
   }, [])
@@ -148,15 +154,26 @@ export default function Paraphraser(props) {
               </svg>) : (<svg className="-ml-1 mr-2 w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" /></svg>)}
               {paraphraseLoading ? 'Paraphrasing...' : 'Paraphrase'}
             </button>
-            <div className="absolute right-0">
-              <button type="button" className="max-w-md flex items-center justify-center px-4 py-2 border border-transparent font-medium rounded-md text-gray-500 hover:text-blue-600 focus:text-blue-600 bg-gray-50" onClick={handleCopyResult}>
-                <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 20 20">
-                  <title>Copy</title>
-                  <path d="M8 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z"></path>
-                  <path d="M6 3a2 2 0 00-2 2v11a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2 3 3 0 01-3 3H9a3 3 0 01-3-3z"></path>
-                </svg>
-                Copy Result
+            <div className="absolute right-0 flex align-middle justify-center">
+              <HideShow show={[...inputText].length > 10 && [...inputText].length < 500}>
+                <button type="button" className="max-w-md flex items-center justify-center px-4 py-2 mx-2 border border-transparent font-medium rounded-md text-red-500 hover:text-red-600 focus:text-red-600 bg-red-50" onClick={handleClear}>
+                  <svg className="-ml-1 mr-2 h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <title>Clear all</title>
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                  </svg>
+                Clear all
               </button>
+              </HideShow>
+              <HideShow show={[...outputText].length > 0}>
+                <button type="button" className="max-w-md flex items-center justify-center px-4 py-2 border border-transparent font-medium rounded-md text-gray-500 hover:text-blue-600 focus:text-blue-600 bg-gray-50" onClick={handleCopyResult}>
+                  <svg className="-ml-1 mr-2 h-6 w-6" fill="currentColor" viewBox="0 0 20 20">
+                    <title>Copy result</title>
+                    <path d="M8 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z"></path>
+                    <path d="M6 3a2 2 0 00-2 2v11a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2 3 3 0 01-3 3H9a3 3 0 01-3-3z"></path>
+                  </svg>
+                Copy result
+              </button>
+              </HideShow>
             </div>
           </div>
         </section>
